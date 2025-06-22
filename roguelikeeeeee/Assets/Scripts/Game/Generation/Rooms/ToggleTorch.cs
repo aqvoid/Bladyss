511a6b42d90a6ab030ad369100cea3abbb9510f0
aqvoid <mindeow35@gmail.com>
2025-06-22 19:28:57 +0300

    Torches mechanic (small update)
    this update is only a foundation for torches mechanic.
    Torches in rooms should light up on clearing the room from mobs/bosses.

--- roguelikeeeeee/Assets/Scripts/Game/Generation/Rooms/ToggleTorch.cs
+++ roguelikeeeeee/Assets/Scripts/Game/Generation/Rooms/ToggleTorch.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
-public class ToggleTorch : MonoBehaviour
+public class ToggleTorches : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite leftExtinguished, rightExtinguished;
+    public Sprite[] leftFlameSprites, rightFlameSprites;
+    public float frameRate = 0.1f;
+
+    private SpriteRenderer[] torchRenderers;
+    private bool isLit = false;
+
+    private void Awake()
     {
-        
+        torchRenderers = GetComponentsInChildren<SpriteRenderer>();
+        SetExtinguished();
+        Debug.Log($"[ToggleTorches] Найдено {torchRenderers.Length} факелов в {gameObject.name}");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetExtinguished()
     {
-        
+        for (int i = 0; i < torchRenderers.Length; i++)
+        {
+            if (i % 2 == 0)
+                torchRenderers[i].sprite = leftExtinguished;
+            else
+                torchRenderers[i].sprite = rightExtinguished;
+        }
+        Debug.Log("[ToggleTorches] Установлены потухшие спрайты");
+    }
+
+    public void TryLightTorches()
+    {
+        if (isLit) return;
+        isLit = true;
+        Debug.Log($"[ToggleTorches] Факела начали зажигаться в {gameObject.name}");
+        StartCoroutine(LightUpWithDelay());
+    }
+
+    private IEnumerator LightUpWithDelay()
+    {
+        for (int i = 0; i < torchRenderers.Length; i++)
+        {
+            //i = every torch sprite slot
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+            StartCoroutine(AnimateTorch(torchRenderers[i], i % 2 == 0 ? leftFlameSprites : rightFlameSprites));
+        }
+        Debug.Log("[ToggleTorches] Все факела зажглись.");
+    }
+
+    private IEnumerator AnimateTorch(SpriteRenderer renderer, Sprite[] frames)
+    {
+        while (true)
+        {
+            for (int i = 0; i < frames.Length; i++)
+            {
+                renderer.sprite = frames[i];
+                yield return new WaitForSeconds(frameRate);
+            }
+        }
     }
 }
